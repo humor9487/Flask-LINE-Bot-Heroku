2eb65c853192f7e70c1aca17a4a52ea5140b3bd8
humor9487 <72068048+humor9487@users.noreply.github.com>
2022-03-11 12:54:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from datetime import datetime
 
 from flask import Flask, abort, request
 
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials as SAC
-
 # https://github.com/line/line-bot-sdk-python
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -15,6 +12,7 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+
 
 @app.route("/", methods=["GET", "POST"])
 def callback():
@@ -33,25 +31,10 @@ def callback():
         return "OK"
 
 
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
 
     # Send To Line
-    reply = TextSendMessage(text=f"紀錄成功{text}")
-    #reply = TextSendMessage(text=f"成功記錄 : {m}月{d}日在{item}項目花費了{cost}元")
+    reply = TextSendMessage(text=f"{get_message}")
     line_bot_api.reply_message(event.reply_token, reply)
-    
-    
-    # ascess Google Sheet
-    Json = 'liquid-streamer-343612-20f96166f6a8.json'  # Json 的單引號內容請改成妳剛剛下載的那個金鑰
-    Url = ['https://spreadsheets.google.com/feeds']
-    # 連結至資料表
-    Connect = SAC.from_json_keyfile_name(Json, Url)
-    GoogleSheets = gspread.authorize(Connect)
-    # 開啟資料表及工作表
-    Sheet = GoogleSheets.open_by_key('15z2LDV9Rr1c7QueeeKQZSWaylEieKo9YJA-vmHLVKNE')  # 這裡請輸入妳自己的試算表代號
-    Sheets = Sheet.sheet1
-    Sheets.append_row([1,2,3,4])
-
