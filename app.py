b8a78c4eb6e407f67a432921f64dfa2b2e0d6178
humor9487 <72068048+humor9487@users.noreply.github.com>
2022-03-14 20:35:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def handle_message(event):
     get_message = event.message.text
 
     # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
+    reply = TextSendMessage(text=f"{get_message}喵~")
     line_bot_api.reply_message(event.reply_token, reply)
 @handler.add(FollowEvent)
 def Welcome(event):
